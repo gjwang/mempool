08b11df4df7a0e6a020405d805364ee7fda6064b
gjwang <guijiewan@gmail.com>
2015-10-12 11:58:51 +0800

    define PoolLock in independance files

---
+++ mempool/PoolLock.cpp
@@ -0,0 +1,9 @@
+//
+//  PoolLock.cpp
+//  mempool
+//
+//  Created by gjwang on 10/12/15.
+//  Copyright (c) 2015 gjwang. All rights reserved.
+//
+
+#include "PoolLock.h"
---
+++ mempool/PoolLock.h
@@ -0,0 +1,14 @@
+//
+//  PoolLock.h
+//  mempool
+//
+//  Created by gjwang on 10/12/15.
+//  Copyright (c) 2015 gjwang. All rights reserved.
+//
+
+#ifndef __mempool__PoolLock__
+#define __mempool__PoolLock__
+
+#include <stdio.h>
+
+#endif /* defined(__mempool__PoolLock__) */
