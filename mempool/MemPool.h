1f9458e66715a2c0ff396f9197ba8a32e4e54d12
gjwang <guijiewan@gmail.com>
2015-10-12 11:59:52 +0800

    complete the last commited

--- mempool/MemPool.h
+++ mempool/MemPool.h
@@ -13,50 +13,9 @@
 #include <iostream>
 #include <vector>
 
+#include "PoolLock.h"
+
 namespace mempool {
-class PoolLock{
-public:
-    explicit PoolLock(){
-        std::cout << "PoolLock() this=" << this << "\n";
-        pthread_mutexattr_t attr;
-        pthread_mutexattr_init(&attr);
-        pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
-        pthread_mutex_init(&_mutex, &attr);
-    }
-    
-    void Lock(){
-        pthread_mutex_lock(&_mutex);
-    }
-    void Unlock(){
-        pthread_mutex_unlock(&_mutex);
-    }
-    
-    virtual ~PoolLock(){
-        std::cout << "~PoolLock()" << "\n";
-    };
-
-private:
-    pthread_mutex_t _mutex;
-};
-
-class  LockScoped {
-public:
-    explicit LockScoped(PoolLock* lock): _lock(lock) {
-        _lock->Lock();
-    }
-    
-    explicit LockScoped(std::shared_ptr<PoolLock> sharelock): _lock(sharelock.get()) {
-        _lock->Lock();
-    }
-    
-    ~LockScoped(){
-        _lock->Unlock();
-    }
-    
-private:
-    PoolLock* _lock;
-};
-
 
 class MemPool{
 public:
